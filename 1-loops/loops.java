6dd3367272fe93099143a7fbc04a1191c8eefd86
Paramveer Singh Udawat <param11veerudawat@gmail.com>
2024-06-17 16:32:33 +0530

    loop questions commited

---
+++ 1-loops/loops.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class loops{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //*WAP That reads a set of integers and print sum of even and odd integers
+        // int n = sc.nextInt();
+        // int sumEven = 0;
+        // int sumOdd = 0;
+        // for(int i = 0; i < n; i++){
+        //     int num = sc.nextInt();
+        //     if(num % 2 == 0){
+        //         sumEven += num;
+        //     }
+        //     else{
+        //         sumOdd += num;
+        //     }
+        // }
+        // System.out.println("Sum of Even numbers: " + sumEven);
+        // System.out.println("Sum of Odd numbers: " + sumOdd);
+    
+        //*Factorial of a number
+        // int n = sc.nextInt();
+        // int fact = 1;
+        // for(int i = 1; i <= n; i++){
+        //     fact *= i;
+        // }
+        // System.out.println("Factorial of " + n + " is: " + fact);
+
+
+        //*Multiplication table of number n
+        // int n = sc.nextInt();
+        // for(int i = 1; i <= 10; i++){
+        //     System.out.println(n + " * " + i + " = " + n * i);
+        // }
+
+
+
+    }
+}
